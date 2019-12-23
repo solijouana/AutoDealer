0b0eb422d54dc62aab45159b5f71547ac9e5db48
sohail <mr_yaboo1@yahoo.com>
2019-12-23 21:38:05 +0330

    Create Dealer

---
+++ AutoDealer.Data/Dealer/Dealer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AutoDealer.Data.BaseType;
+
+namespace AutoDealer.Data.Dealer
+{
+    public class Dealer : BaseEntity
+    {
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} خود را وارد کنید")]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+        [Display(Name = "تاریخ عضویت")]
+        [Required(ErrorMessage = "لطفا {0} خود را وارد کنید")]
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public string ImageName { get; set; }
+
+    }
+}
