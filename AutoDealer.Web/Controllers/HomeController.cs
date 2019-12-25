25bb92104fa3d4f2a6beecb8413a2966f5ae6c09
solijouana <mr_yaboo1@yahoo.com>
2019-12-25 12:53:11 +0330

    Add Template

---
+++ AutoDealer.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace AutoDealer.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Slider()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Search()
+        {
+            return PartialView();
+        }
+
+        public ActionResult LastCars()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Banner()
+        {
+            return PartialView();
+        }
+
+    }
+}
