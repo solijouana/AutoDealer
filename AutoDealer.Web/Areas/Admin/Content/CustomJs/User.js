ce18e525c1438e4708838729a236ae15b250c5f3
solijouana <mr_yaboo1@yahoo.com>
2019-12-28 16:23:08 +0330

    Add Admin Panel

---
+++ AutoDealer.Web/Areas/Admin/Content/CustomJs/User.js
@@ -0,0 +1,21 @@
+﻿
+function DeleteUser(userId) {
+    $.get("/Admin/Users/Delete/" + userId).done(function (res) {
+        if (res.status === "Done") {
+            location.reload();
+        } else {
+            alert(res.status);
+        }
+    });
+
+};
+
+function ReturnUser(userId) {
+    $.get("/Admin/Users/Return/" + userId).done(function (res) {
+        if (res.status === "Done") {
+            location.reload();
+        } else {
+            alert(res.status);
+        }
+    });
+};
