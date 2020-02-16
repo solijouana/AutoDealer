074e25b84844977c97e1f93b233e77f29e83ec7c
solijouana <mr_yaboo1@yahoo.com>
2020-02-16 14:39:17 +0330

    add migration for change data type productionDate

---
+++ AutoDealer.Repository/Migrations/202002161102435_change datatype trip.cs
@@ -0,0 +1,18 @@
+﻿namespace AutoDealer.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changedatatypetrip : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Cars", "ProductionDate", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Cars", "ProductionDate", c => c.String(nullable: false));
+        }
+    }
+}
